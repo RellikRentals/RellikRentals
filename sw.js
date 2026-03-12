097b919c588ef6bdc4b2d09a2779b12d88b8b195
RellikRentals <unfused@live.ca>
2026-03-12 13:03:17 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // RellikRentals Service Worker v1.0
 // Handles push notifications via Firebase Cloud Messaging
 
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.14.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.14.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyAXKtBIVFazSDS8oY9tj8mmdIgGfiQgLr4",
